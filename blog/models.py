f9ca525ab35c3d7c36b0f763f6e6a2225d660835
Carmen <carmencantudo@gmail.com>
2022-12-20 17:48:56 +0000

    Add comments model

--- blog/models.py
+++ blog/models.py
@@ -32,3 +32,22 @@ class Post(models.Model):
     # Return the total number of likes on a post
     def number_of_likes(self):
         return self.likes.count()
+
+
+# Comment Model
+class Comment(models.Model):
+    post = models.ForeignKey(
+        Post, on_delete=models.CASCADE, related_name="comments")
+    name = models.CharField(max_length=80, unique=True)
+    email = models.EmailField()
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=False)
+
+    # Order posts on created field (-: descending order)
+    class Meta:
+        ordering = ["-created_on"]
+
+    # Return a string representation of an object
+    def __str__(self):
+        return f"Comment {self.body} by {self.name}"
