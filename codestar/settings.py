41d650cb578207b475f56057f74b7f097947c885
Carmen <carmencantudo@gmail.com>
2022-12-26 17:18:43 +0000

    Add django.contrib.sites and allauth to installed apps

--- codestar/settings.py
+++ codestar/settings.py
@@ -41,6 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'django.contrib.sites',
+    'allauth',
     'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
